a60460e699932044dd470b16a332bafee1c160e6
Annoyingpanthera <122933423+Annoyingpanthera@users.noreply.github.com>
2023-08-10 17:00:00 +0200

    init:reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    const reversedString = String(number).split('').reverse().join('');
+    const reversedAbsolute = parseInt(reversedString);
+    const absoluteValue = Math.abs(reversedAbsolute);
+    return absoluteValue;
+  }
+
